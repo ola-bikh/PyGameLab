77c25b62b3a87f370a19c31f5de6220ad14f0a97
Aleksandra Bikhit <144633285+ola-bikh@users.noreply.github.com>
2025-01-26 00:22:22 +0100

    Created class board

--- main.py
+++ main.py
@@ -10,20 +10,6 @@ CZERWONY = (255, 0, 0)
 NIEBIESKI = (0, 0, 255)
 
 
-class Board:
-    def __init__(self, size, width, height):
-        self.size = size
-        self.cell_width = width // size
-        self.width = width
-        self.height = height
-
-    def draw(self, ekran):
-        for i in range(self.size):
-            # Górna ścieżka
-            pygame.draw.rect(ekran, CZARNY, (i * self.cell_width, self.height // 3, self.cell_width, 50), 1)
-            # Dolna ścieżka
-            pygame.draw.rect(ekran, CZARNY, (i * self.cell_width, (self.height // 3) * 2, self.cell_width, 50), 1)
-
 
 class Die:
     def __init__(self, x, y, size):
